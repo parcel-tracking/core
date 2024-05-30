d0681ab33e986d3c84f8128eb73df7dfa72bcfd7
CheolJe <me@falsy.me>
2024-05-30 21:15:21 +0900

    feat: modifying Carrier entities and DTOs

--- src/core/domains/entities/Carrier.ts
+++ src/core/domains/entities/Carrier.ts
@@ -3,8 +3,8 @@ import ICarrier from "./interfaces/ICarrier"
 export default class Carrier implements ICarrier {
   constructor(
     readonly no: number,
-    readonly id: string,
     readonly name: string,
+    readonly displayName: string,
     readonly isCrawlable: boolean,
     readonly isPopupEnabled: boolean,
     readonly popupURL: string
