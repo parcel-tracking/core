9fc44c21626602cca34a57bd58ec4ed6c717168d
CheolJe <me@falsy.me>
2024-06-01 22:02:28 +0900

    feat: modify your use case

--- src/core/repositories/interfaces/ITrackerRepository.ts
+++ src/core/repositories/interfaces/ITrackerRepository.ts
@@ -1,10 +1,11 @@
+import ICarrier from "../../domains/entities/interfaces/ICarrier"
 import ITracker from "../../domains/entities/interfaces/ITracker"
 import IDeliveryDTO from "../../dtos/interfaces/IDeliveryDTO"
 import ILayerDTO from "../../dtos/interfaces/ILayerDTO"
 
 export default interface ITrackerRepository {
   getDelivery?(
-    carrierId: string,
+    carrier: ICarrier,
     trackingNumber: string
   ): Promise<ILayerDTO<IDeliveryDTO>>
   getTrackers?(): Promise<ILayerDTO<ITracker[]>>
